5c1192e2ab35d2ba3cd733bb9eaa956ae7e90ecb
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-06-04 13:59:39 +0100

    Test different llms speed and output quality

---
+++ test_ds_14b.py
@@ -0,0 +1,17 @@
+from tune_analysis import analyse_tune
+from model_prompt import generate_description
+import time
+
+start_time = time.time()
+
+file = './tunes/the_lion_king.mp3'
+audio_info = analyse_tune(file)
+print(audio_info)
+
+llm = 'deepseek-r1:14b'
+print(generate_description(audio_info, llm))
+
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Total time taken: {elapsed_time:.2f} seconds")
