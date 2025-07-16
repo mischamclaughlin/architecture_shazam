27e0c7b7b7d1dbd1a82e41c736db1933c22031aa
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-07-16 16:32:25 +0100

    Script to use MusicBrainz API to extract artists origin to help tailor end result

---
+++ musicbrainz_api_test.py
@@ -0,0 +1,31 @@
+# ./musicbrainz_api_test.py
+import musicbrainzngs
+from wikipedia_api_test import get_origin_wikipedia
+
+musicbrainzngs.set_useragent("MyApp", "0.1", "you@example.com")
+
+
+def get_country_and_area(artist_name):
+    try:
+        query = f"{artist_name}"
+        results = musicbrainzngs.search_artists(query=query, limit=1)["artist-list"]
+
+        if not results:
+            raise LookupError(f"No MusicBrainz artist found for {artist_name!r}")
+
+        mbid = results[0]["id"]
+        details = musicbrainzngs.get_artist_by_id(mbid)
+        art = details["artist"]
+        country = art.get("country")
+        area_name = art.get("area", {}).get("name")
+
+        return country, area_name
+    except (musicbrainzngs.WebServiceError, KeyError) as e:
+        print(f"[MusicBrainz Error] {e}")
+
+    origin = get_origin_wikipedia(artist_name)
+    return origin
+
+
+print(get_country_and_area("Teresa Teng"))
+print(get_country_and_area("Ludwig van Beethoven"))
