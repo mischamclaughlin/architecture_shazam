e5a80f279ac6f6376e1a687ade36e8b02b333bbe
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-07-17 12:58:41 +0100

    Hide keys

--- spotify_api_test.py
+++ spotify_api_test.py
@@ -1,20 +1,26 @@
 # ./spotify_api_test.py
 import os
+from dotenv import load_dotenv
 from spotipy import Spotify
 from spotipy.oauth2 import SpotifyClientCredentials
 
 
-os.environ["SPOTIPY_CLIENT_ID"] = "183e7f5c41ba431898d97e617e8b9d82"
-os.environ["SPOTIPY_CLIENT_SECRET"] = "c059dfd0c31440599d418b85a06a6431"
+load_dotenv()
 
-auth_manager = SpotifyClientCredentials()
+
+client_id = os.environ["SPOTIPY_CLIENT_ID"]
+client_secret = os.environ["SPOTIPY_CLIENT_SECRET"]
+
+
+auth_manager = SpotifyClientCredentials(
+    client_id=client_id, client_secret=client_secret
+)
 sp = Spotify(auth_manager=auth_manager)
 
-# result = sp.search(q="track:Tian Mi Mi", type="track", limit=1)
-result = sp.search(q="track:Shape of You", type="track", limit=1)
+result = sp.search(q="track:Tian Mi Mi", type="track", limit=1)
 track = result["tracks"]["items"][0]
 
-artist_id = track['artists'][0]['id']
+artist_id = track["artists"][0]["id"]
 artist = sp.artist(artist_id)
 
 print(f"Track: {track['name']}")
