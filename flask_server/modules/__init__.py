3f7a5351bbeb869ec2a43db2cdd9ff02dd697952
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-07-18 16:14:36 +0100

    Add classes to module

--- flask_server/modules/__init__.py
+++ flask_server/modules/__init__.py
@@ -2,3 +2,13 @@
 from .song_info import ExtractSongInfo
 from .image_generation import GenerateImage
 from .llm_description import GenerateLLMDescription
+
+
+# API services
+from .services.acrcloud_service import ACRCloudService
+from .services.musicbrainz_service import MusicBrainzService
+from .services.wiki_service import WikiService
+
+# Analysis Tools
+from .analysis.features import AudioFeatureExtractor
+from .analysis.classifiers import GenreClassifier, InstrumentClassifier
