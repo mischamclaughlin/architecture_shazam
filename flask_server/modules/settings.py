47708d5e5bbc803533a795c418adb95f646388e9
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-07-18 16:13:12 +0100

    Clean setting script to pull API keys securely

---
+++ flask_server/modules/settings.py
@@ -0,0 +1,21 @@
+# ./flask_server/modules/settings.py
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # ACRCloud
+    ACRCLOUD_HOST: str
+    ACRCLOUD_KEY: str
+    ACRCLOUD_SECRET: str
+
+    # Spotify
+    SPOTIPY_CLIENT_ID: str
+    SPOTIPY_CLIENT_SECRET: str
+
+    # Wikipedia
+    WIKI_USER_AGENT: str = "ArchitectureShazamScript/0.1 (you@example.com)"
+    WIKI_API_URL: str = "https://en.wikipedia.org/w/api.php"
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
