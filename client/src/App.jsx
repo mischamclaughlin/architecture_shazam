1cb1bac0b0ac2c7801926df9aeb365f96ee22c39
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-08-06 22:51:44 +0100

    Organise global page layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,4 @@
+// ./client/src/App.jsx
 import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
@@ -5,8 +6,10 @@ import HomePage from './pages/HomePage.jsx';
 import RegisterPage from './pages/RegisterPage.jsx';
 import LoginPage from './pages/LoginPage.jsx';
 import NotFoundPage from './pages/NotFoundPage.jsx';
+import GalleryPage from './pages/GalleryPage.jsx';
 
 import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 
 import useLogout from './hooks/useLogout.jsx'
 
@@ -26,12 +29,17 @@ function App() {
     <div className='container'>
       <Navbar currentUser={user} onLogout={logout} />
 
-      <Routes>
-        <Route path='/' element={<HomePage user={user} />} />
-        <Route path='/login' element={<LoginPage onLogin={setUser} />} />
-        <Route path='/register' element={<RegisterPage />} />
-        <Route path='*' element={<NotFoundPage />} />
-      </Routes>
+      <main className='content'>
+        <Routes>
+          <Route path='/' element={<HomePage user={user} />} />
+          <Route path='/gallery' element={<GalleryPage user={user} />} />
+          <Route path='/login' element={<LoginPage onLogin={setUser} />} />
+          <Route path='/register' element={<RegisterPage />} />
+          <Route path='*' element={<NotFoundPage />} />
+        </Routes>
+      </main>
+
+      <Footer />
     </div>
   );
 }
