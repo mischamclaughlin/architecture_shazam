3adb08147d94c43672f5e43faa36cef74bf3739d
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-08-06 22:58:32 +0100

    Add latest picture display component

---
+++ client/src/components/LatestImage.jsx
@@ -0,0 +1,31 @@
+// ./client/src/components/LatestImage.jsx
+import React, { useEffect } from 'react';
+import { useLatestImage } from '../hooks/useLatestImage';
+import ImageWithActions from './ImageWithActions';
+import './LatestImage.css';
+
+export default function LoadImage({ refreshKey }) {
+    const { image: img, loading, reload } = useLatestImage();
+
+    useEffect(() => {
+        reload();
+    }, [refreshKey, reload]);
+
+    if (loading) return <p>Loading your latest image…</p>;
+    if (!img) return <p>No images generated yet.</p>;
+
+    return (
+        <div className='latest-image-area'>
+            <h2 className='title'>Latest Image</h2>
+            <div className='image'>
+                <ImageWithActions
+                    key={img.id}
+                    id={img.id}
+                    src={img.url}
+                    alt={img.filename}
+                    onDeleted={() => reload()}
+                />
+            </div>
+        </div>
+    );
+}
