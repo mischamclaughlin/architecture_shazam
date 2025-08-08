657f71ee6fe4140aaefa9d4e9b627d9ee84555a9
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-08-08 16:30:35 +0100

    Add model to paths when REACT is interacting with backend

--- client/vite.config.js
+++ client/vite.config.js
@@ -14,6 +14,10 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
       },
+      '/static/models': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      }
     },
   },
 })
