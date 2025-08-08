9d5c530b06f1555066e18e1f39e27bab5392f2d6
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-08-08 16:29:43 +0100

    Edit to allow more info to be passed when generate button is clicked: type of generation and building type

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,7 +4,8 @@ import DragDropUpload from '../components/DragDropUpload';
 import SpotifySearch from '../components/SongSearch';
 import StatusMessage from '../components/StatusInfo';
 import ErrorStatusMessage from '../components/ErrorStatus';
-import LoadImage from '../components/LatestImage';
+import LatestImage from '../components/LatestImage';
+import LatestModel from '../components/LatestModel';
 
 import { useGenerateImage } from '../hooks/useGenerateImage';
 
@@ -15,6 +16,7 @@ export default function HomePage() {
     const [snippetInfo, setSnippetInfo] = useState(null);
     const [query, setQuery] = useState('');
     const [refreshKey, setRefreshKey] = useState(0);
+    const [option, setOption] = useState('house')
 
     const { status, errorStatus, generate, generateFromUrl } = useGenerateImage();
 
@@ -24,12 +26,16 @@ export default function HomePage() {
         setFile(null);
     };
 
-    const handleGenerateClick = async () => {
-        if (snippetInfo?.preview_url) {
-            await generateFromUrl(snippetInfo);
-        } else if (file) {
-            await generate({ file, title: query });
-        }
+    const handleGenerateClick = async (type, option) => {
+        try {
+            if (snippetInfo?.preview_url) {
+                await generateFromUrl(type, option, snippetInfo);
+            } else if (file) {
+                await generate(type, option, { file, title: query });
+            }
+        } catch (e) {
+            console.error('Generation failed: ', e);
+        };
 
         setFile(null);
         setSnippetInfo(null);
@@ -45,27 +51,34 @@ export default function HomePage() {
 
     return (
         <div className="home-page">
-            <details className='image-generation' open>
-                <summary>Image Generation</summary>
-                <div className="file-interaction-area">
-                    <div className="song-search-area">
-                        <h2>Song Search</h2>
-                        <SpotifySearch
-                            value={query}
-                            onChange={setQuery}
-                            onResult={handleTrackLookup}
-                        />
-                    </div>
+            <div className="file-interaction-area">
+                <div className="song-search-area">
+                    <h2>Song Search</h2>
+                    <SpotifySearch
+                        value={query}
+                        onChange={setQuery}
+                        onResult={handleTrackLookup}
+                    />
+                </div>
 
-                    <div className="file-upload-area">
-                        <h2>Upload File</h2>
-                        <DragDropUpload
-                            onFileSelect={setFile}
-                        />
-                    </div>
-
+                <div className="file-upload-area">
+                    <h2>Song Upload</h2>
+                    <DragDropUpload
+                        onFileSelect={setFile}
+                    />
+                </div>
+                <div className='select-area'>
+                    <label className='title-selector'>Building Type:</label>
+                    <select className='option-selector' value={option} onChange={e => setOption(e.target.value)}>
+                        <option value="house">house</option>
+                        <option value="skyscraper">skyscraper</option>
+                        <option value="apartments">apartments</option>
+                        <option value="library">library</option>
+                    </select>
+                </div>
+                <div className='btn-area'>
                     <button
-                        onClick={handleGenerateClick}
+                        onClick={() => handleGenerateClick('image', option)}
                         disabled={
                             (!file && !(snippetInfo?.preview_url)) ||
                             status.includes('…')
@@ -74,16 +87,37 @@ export default function HomePage() {
                     >
                         {status && status !== 'Done!' ? 'Generating…' : 'Generate Image'}
                     </button>
+                    <button
+                        onClick={() => handleGenerateClick('model', option)}
+                        disabled={
+                            (!file && !(snippetInfo?.preview_url)) ||
+                            status.includes('…')
+                        }
+                        className="generate-button"
+                    >
+                        {status && status !== 'Done!' ? 'Generating…' : 'Generate Model'}
+                    </button>
+                </div>
 
-                    {errorStatus
-                        ? <ErrorStatusMessage status={errorStatus} />
-                        : <StatusMessage status={status} />
-                    }
-                </div>
-                <div>
-                    <LoadImage key={refreshKey} />
-                </div>
-            </details>
-        </div>
+                {errorStatus
+                    ? <ErrorStatusMessage status={errorStatus} />
+                    : <StatusMessage status={status} />
+                }
+            </div>
+            <div className='latest-area'>
+                <details className='view-generation' open>
+                    <summary>Latest Image</summary>
+                    <div>
+                        <LatestImage key={refreshKey} />
+                    </div>
+                </details>
+                <details className='view-generation' open>
+                    <summary>Latest Model</summary>
+                    <div>
+                        <LatestModel key={refreshKey} />
+                    </div>
+                </details>
+            </div>
+        </div >
     );
 }
