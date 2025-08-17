384eb4d10ed50fa224c6f879a6f5d25f1572b3ed
hub-mm <165568972+hub-mm@users.noreply.github.com>
2025-08-17 19:21:39 +0100

    Delete no longer used files

--- download_mistral.py
+++
@@ -1,8 +0,0 @@
-from huggingface_hub import snapshot_download
-
-# this will download into ./models/mistral-7b-instruct-v0.3
-snapshot_download(
-    repo_id="mistralai/Mistral-7B-Instruct-v0.3",
-    local_dir="models/mistral-7b-instruct-v0.3",
-    use_auth_token=True,
-)
